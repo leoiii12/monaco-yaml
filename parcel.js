4f2bf3917c2424650742b7210a8cd715b7445e67
Leo Choi <choimankin@gmail.com>
2019-07-09 22:21:22 +0800

    Updated bundles

--- parcel.js
+++ parcel.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const parcelBundler = require('parcel-bundler');
 
-const outDir = 'dist';
+const devDir = 'dist/dev';
+const minDir = 'dist/min';
+
 const options = {
-  outDir: outDir,
+  outDir: devDir,
   watch: false,
   cache: true,
   cacheDir: '.cache',
@@ -18,26 +20,37 @@ const options = {
 };
 
 const entries = [
-  { entry: 'out/monaco.contribution.js', global: 'vs/language/yaml/monaco.contribution' },
-  { entry: 'out/yamlWorker.js', global: 'vs/language/yaml/yamlWorker' },
+  { entry: 'out/monaco.contribution.js', global: 'vs/language/yaml/monaco.contribution', },
+  { entry: 'out/yamlWorker.js', global: 'vs/language/yaml/yamlWorker', },
 ];
 
-const hi = async () => {
+const bundle = async () => {
   for (const entry of entries) {
     options.global = entry.global;
+    options.minify = false;
+    options.scopeHoist = false;
+    options.outDir = devDir;
 
     const bundler = new parcelBundler(entry.entry, options);
-    await bundler
-      .bundle()
+    await bundler.bundle()
       .then((bundle) => {
-        fs.writeFileSync(`${outDir}/${bundle.entryAsset.id}.bundle`, Array.from(bundle.assets).map(a => `${a.name},${a.bundledSize}`).join('\n'));
+        fs.writeFileSync(`${options.outDir}/${bundle.entryAsset.id}.bundle`, Array.from(bundle.assets).map(a => `${a.name},${a.bundledSize}`).join('\n'));
       });
+  }
+
+  for (const entry of entries) {
+    options.global = entry.global;
+    options.minify = true;
+    options.scopeHoist = true;
+    options.outDir = minDir;
+
+    const bundler = new parcelBundler(entry.entry, options);
+    await bundler.bundle();
   }
 };
 
-hi()
-  .then(() => {
-  });
+bundle().then(() => {
+});
 
 
 
